f6e54c5434ac07ccac0a7c835a1a8e19cdc0ce82
Sean <sean@mess.org>
2019-12-02 10:47:06 +0000

    Teach --verbose to show what files are generated and for what target
    Signed-off-by: Sean <sean@mess.org>

--- src/abi/mod.rs
+++ src/abi/mod.rs
@@ -4,14 +4,22 @@ use resolver::{Contract, Target};
 pub mod ethabi;
 mod substrate;
 
-pub fn generate_abi(contract: &Contract) -> (Vec<u8>, &'static str) {
+pub fn generate_abi(contract: &Contract, verbose: bool) -> (Vec<u8>, &'static str) {
     match contract.target {
         Target::Burrow => {
+            if verbose {
+                eprintln!("info: Generating Burrow ABI for contract {}", contract.name);
+            }
+
             let abi = ethabi::gen_abi(contract);
 
             (serde_json::to_string(&abi).unwrap().as_bytes().to_vec(), "abi")
         },
         Target::Substrate => {
+            if verbose {
+                eprintln!("info: Generating Substrate ABI for contract {}", contract.name);
+            }
+
             let abi = substrate::gen_abi(contract);
 
             (serde_json::to_string_pretty(&abi).unwrap().as_bytes().to_vec(), "json")
