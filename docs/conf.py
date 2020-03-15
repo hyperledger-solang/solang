31bda00d2bd423dc582239ec9aa56fda9de113a4
Sean Young <sean@mess.org>
2020-03-15 09:20:43 +0000

    version 0.1.1
    Signed-off-by: Sean Young <sean@mess.org>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Sean Young <sean@mess.org>'
 author = 'Sean Young <sean@mess.org>'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
