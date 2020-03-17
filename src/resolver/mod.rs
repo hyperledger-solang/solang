3f19de03224bba401c02eeb4151cc82ab5dfd070
Sean Young <sean@mess.org>
2020-03-17 07:01:22 +0000

    Allocate bytes/string with new
    Signed-off-by: Sean Young <sean@mess.org>

--- src/resolver/mod.rs
+++ src/resolver/mod.rs
@@ -113,10 +113,10 @@ impl Type {
         }
     }
 
-    /// Give the type of an array after dereference. This can only be used on
-    /// array types and will cause a panic otherwise.
+    /// Give the type of an memory array after dereference.
     pub fn array_deref(&self) -> Self {
         match self {
+            Type::String | Type::DynamicBytes => Type::Uint(8),
             Type::Ref(t) => t.array_deref(),
             Type::Array(ty, dim) if dim.len() > 1 => {
                 Type::Array(ty.clone(), dim[..dim.len() - 1].to_vec())
