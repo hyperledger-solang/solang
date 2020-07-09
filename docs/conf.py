d40b4e4914e25783f056760f6f3f0c4248eff747
Sean Young <sean@mess.org>
2020-07-09 13:49:53 +0100

    Timestamp should be seconds
    Signed-off-by: Sean Young <sean@mess.org>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Sean Young <sean@mess.org>'
 author = 'Sean Young <sean@mess.org>'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
