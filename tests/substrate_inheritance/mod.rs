7d206b7cd854ee0a9adc0c8ef9d169bdb8758612
Sean Young <sean@mess.org>
2020-08-15 13:04:33 +0100

    Test case: can we override function w/ multiple bases in same contract
    This does not work by default, since solang warns that the virtual function
    should be override.
    
    Signed-off-by: Sean Young <sean@mess.org>

--- tests/substrate_inheritance/mod.rs
+++ tests/substrate_inheritance/mod.rs
@@ -1159,4 +1159,68 @@ fn multiple_override() {
         first_error(ns.diagnostics),
         "function ‘foo’ should specify override list ‘override(bar2,bar)’"
     );
+
+    let ns = parse_and_resolve(
+        r#"
+        contract base is bar, bar2 {
+            function foo(uint64 a) virtual internal returns (uint64) {
+                return a + 104;
+            }
+
+            function foo(uint64 a) override(bar,bar2) internal returns (uint64) {
+                return a + 102;
+            }
+        }
+
+        contract bar {
+            function foo(uint64 a) virtual internal returns (uint64) {
+                return a + 102;
+            }
+        }
+
+        contract bar2 {
+            function foo(uint64 a) virtual internal returns (uint64) {
+                return a + 103;
+            }
+        }
+        "#,
+        Target::Substrate,
+    );
+
+    assert_eq!(
+        first_error(ns.diagnostics),
+        "function ‘foo’ should specify override list ‘override(bar2,bar)’"
+    );
+
+    let ns = parse_and_resolve(
+        r#"
+        contract base is bar, bar2 {
+            function foo(uint64 a) override(bar,bar2) internal returns (uint64) {
+                return a + 104;
+            }
+
+            function foo(uint64 a) override(bar,bar2) internal returns (uint64) {
+                return a + 102;
+            }
+        }
+
+        contract bar {
+            function foo(uint64 a) virtual internal returns (uint64) {
+                return a + 102;
+            }
+        }
+
+        contract bar2 {
+            function foo(uint64 a) virtual internal returns (uint64) {
+                return a + 103;
+            }
+        }
+        "#,
+        Target::Substrate,
+    );
+
+    assert_eq!(
+        first_error(ns.diagnostics),
+        "function ‘foo’ overrides function in same contract"
+    );
 }
