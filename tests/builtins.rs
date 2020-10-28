dbf1a7d531f55c30caf7506383406a906ddf242e
Sean Young <sean@mess.org>
2020-10-28 12:24:34 +0000

    Not all builtins have prototype entries yet
    Signed-off-by: Sean Young <sean@mess.org>

--- tests/builtins.rs
+++ tests/builtins.rs
@@ -5,9 +5,12 @@ use solang::sema::builtin;
 
 #[test]
 fn builtin_prototype() {
-    let p = builtin::get_prototype(ast::Builtin::Timestamp);
+    let p = builtin::get_prototype(ast::Builtin::Timestamp).unwrap();
 
     assert_eq!(p.namespace, Some("block"));
     assert_eq!(p.name, "timestamp");
     assert!(p.args.is_empty());
+
+    // there is no entry for some builtins
+    assert!(builtin::get_prototype(ast::Builtin::ArrayPush).is_none());
 }
