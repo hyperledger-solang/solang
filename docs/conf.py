95552e09b388e0644d1b9537984f4be7a604da48
Sean Young <sean@mess.org>
2020-11-09 14:31:22 +0000

    Substrate 2.0 and Solidity Function Types
    - Function types are implemented
    - An experimental [Solana](https://solana.com/) target has been added
    - Binaries are generated for Mac
    
    - The Substrate target requires Substrate 2.0
    - Various bug fixes (see git log)
    
    Signed-off-by: Sean Young <sean@mess.org>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, 2020 Sean Young <sean@mess.org>'
 author = 'Sean Young <sean@mess.org>'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = '0.1.5'
 
 
 # -- General configuration ---------------------------------------------------
