2a231aae45fc9dd9793f4c987a53aa7ed4e4b865
Marcin Górny <marcin@web3.foundation>
2021-01-18 18:56:25 +0100

    Expand constant folding unit tests
    Signed-off-by: Marcin Górny <marcin@web3.foundation>

--- tests/codegen_testcases/const.sol
+++ tests/codegen_testcases/const.sol
@@ -1,8 +1,25 @@
 // RUN: --emit cfg
 contract c {
+// BEGIN-CHECK: c::test
 	function test() public pure returns (int32) {
 		int32 x = 102;
 // CHECK: return int32 102
+		return x;
+	}
+
+// BEGIN-CHECK: c::add
+	function add() public pure returns (int32) {
+		int32 x = 5;
+		x += 3;
+// CHECK: return int32 8
+		return x;
+	}
+
+// BEGIN-CHECK: c::power
+	function power() public pure returns (uint32) {
+		uint32 x = 2;
+		x = x**4;
+// CHECK: return uint32 16
 		return x;
 	}
 }
---
+++ tests/codegen_testcases/const_fail.sol
@@ -0,0 +1,10 @@
+// RUN: --emit cfg
+contract c {
+	function divide_zero() public pure returns (uint32) {
+		uint32 x = 2;
+		x = x / 0;
+// FAIL: divide by zero
+		return x;
+	}
+
+}
