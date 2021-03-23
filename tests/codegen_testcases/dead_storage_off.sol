00db951cb4cf0cb8b9e6d5edad98857214d61896
Marcin Górny <marcin@web3.foundation>
2021-03-23 20:04:05 +0100

    Make sure to include tests for --no-dead-storage flag on
    Signed-off-by: Marcin Górny <marcin@web3.foundation>

---
+++ tests/codegen_testcases/dead_storage_off.sol
@@ -0,0 +1,14 @@
+// RUN: --no-dead-storage --emit cfg
+contract nodeadstorage {
+    int a;
+
+    // simple test. Two references to "a" must result in loadstorage twice with --no-dead-storage flag
+
+// BEGIN-CHECK: nodeadstorage::test1
+	function test1() public view returns (int) {
+        return a + a;
+	}
+// CHECK: load storage slot(uint256 0) ty:int256
+// CHECK: load storage slot(uint256 0) ty:int256
+
+}
