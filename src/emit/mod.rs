c383acf75c0ea9b0bf18215ead301606c7858df3
Sean Young <sean@mess.org>
2021-11-13 23:00:59 +0000

    Ensure all solana syscalls are declared external in the llvm IR
    Currently we have declarations like:
    
    declare internal @sol_set_return_data(i8*, i64) local_unnamed_addr
    
    This is not valid because there is no body.
    
    Signed-off-by: Sean Young <sean@mess.org>

--- src/emit/mod.rs
+++ src/emit/mod.rs
@@ -5348,7 +5348,20 @@ impl<'a> Binary<'a> {
         while let Some(f) = func {
             let name = f.get_name().to_str().unwrap();
 
-            if !name.starts_with("llvm.") && export_list.iter().all(|e| e != &name) {
+            if !name.starts_with("llvm.")
+                && export_list.iter().all(|e| {
+                    // symbols may get renamed foo.1 or foo.2, also export those
+                    if let Some(tail) = name.strip_prefix(e) {
+                        if let Some(no) = tail.strip_prefix('.') {
+                            no.parse::<u32>().is_ok()
+                        } else {
+                            tail.is_empty()
+                        }
+                    } else {
+                        false
+                    }
+                })
+            {
                 f.set_linkage(Linkage::Internal);
             }
 
