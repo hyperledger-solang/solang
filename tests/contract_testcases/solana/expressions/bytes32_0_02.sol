1367dbd12446bb76ce5352cecbf2df2ae2eed3d1
Sean Young <sean@mess.org>
2021-12-13 15:36:06 +0000

    Fixed length hex literal initializer can be any length as long as its zero
    Signed-off-by: Sean Young <sean@mess.org>

--- tests/contract_testcases/solana/expressions/bytes32_0_02.sol
+++ tests/contract_testcases/solana/expressions/bytes32_0_02.sol
@@ -1,5 +1,13 @@
 
         contract foo {
+            bytes32 public x = 0x00;
+            bytes32 public y = 0x00_00_00;
+            bytes3 public z1 = 0x10_00_00;
+            // same length
+            bytes3 public z2 = 0x00_00_00;
+            // longer
+            bytes3 public z3 = 0x00_00_00_00;
+
             function b32() public pure returns (bytes32 r) {
                 r = bytes32(-1);
             }
