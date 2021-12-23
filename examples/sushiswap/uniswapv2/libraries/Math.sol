a9483afba5cedb3410a56ef4cf8ca8334e7c4f09
soloway2010 <soloway2010@yandex.ru>
2021-12-23 14:30:46 +0300

    Removed unnecessary contracts

---
+++ examples/sushiswap/uniswapv2/libraries/Math.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity =0.6.12;
+
+// a library for performing various math operations
+
+library Math {
+    function min(uint x, uint y) internal pure returns (uint z) {
+        if (x < y) {
+            z = x;
+        } else {
+            z = y;
+        }
+    }
+
+    // babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+}
---
+++ examples/sushiswap/uniswapv2/libraries/SafeMath.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity =0.6.12;
+
+// a library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
+
+library SafeMathUniswap {
+    function add(uint x, uint y) internal pure returns (uint z) {
+        z = x + y;
+
+        require(z >= x, 'ds-math-add-overflow');
+    }
+
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        z = x - y;
+
+        require(z <= x, 'ds-math-sub-underflow');
+    }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        if (y == 0) {
+            return 0;
+        }
+
+        z = x * y;
+
+        require(z / y == x, 'ds-math-mul-overflow');
+    }
+}
