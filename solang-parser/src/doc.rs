ca4bdd3eec699b003eb8f46ae53ced16aafc1be7
Alexey Shekhirin <a.shekhirin@gmail.com>
2022-01-06 13:43:02 +0300

    refactor a bit
    Signed-off-by: Alexey Shekhirin <a.shekhirin@gmail.com>

--- solang-parser/src/doc.rs
+++ solang-parser/src/doc.rs
@@ -9,26 +9,27 @@ fn to_lines<'a>(
     let mut res = Vec::new();
 
     for (start, ty, comment) in comments.iter() {
+        let mut grouped_comments = Vec::new();
+
         match ty {
-            CommentType::Line => res.push((*ty, vec![(*start, comment.trim())])),
+            CommentType::Line => grouped_comments.push((*start, comment.trim())),
             CommentType::Block => {
                 let mut start = *start;
-                let mut block_comments = Vec::new();
 
                 for s in comment.lines() {
                     if let Some((i, _)) = s
                         .char_indices()
                         .find(|(_, ch)| !ch.is_whitespace() && *ch != '*')
                     {
-                        block_comments.push((start + i, s[i..].trim_end()));
+                        grouped_comments.push((start + i, s[i..].trim_end()));
                     }
 
                     start += s.len();
                 }
-
-                res.push((*ty, block_comments));
             }
         }
+
+        res.push((*ty, grouped_comments));
     }
 
     res
@@ -41,7 +42,7 @@ pub fn tags(lines: &[(usize, CommentType, &str)]) -> Vec<DocComment> {
 
     let lines = to_lines(lines);
     for (ty, comment_lines) in lines {
-        let mut tags_buffer = Vec::new();
+        let mut single_tags = Vec::new();
 
         for (start_offset, line) in comment_lines {
             let mut chars = line.char_indices().peekable();
@@ -62,16 +63,16 @@ pub fn tags(lines: &[(usize, CommentType, &str)]) -> Vec<DocComment> {
                 }
 
                 // tag value
-                tags_buffer.push(SingleDocComment {
+                single_tags.push(SingleDocComment {
                     offset: tag_start,
                     tag: line[tag_start + 1..tag_end + 1].to_owned(),
                     value: line[tag_end + 1..].trim().to_owned(),
                 });
-            } else if tags_buffer.len() > 0 || tags.len() > 0 {
+            } else if single_tags.len() > 0 || tags.len() > 0 {
                 let line = line.trim();
                 if !line.is_empty() {
-                    let single_doc_comment = if let Some(tag_buffered) = tags_buffer.last_mut() {
-                        Some(tag_buffered)
+                    let single_doc_comment = if let Some(single_tag) = single_tags.last_mut() {
+                        Some(single_tag)
                     } else if let Some(tag) = tags.last_mut() {
                         match tag {
                             DocComment::Line { comment } => Some(comment),
@@ -87,7 +88,7 @@ pub fn tags(lines: &[(usize, CommentType, &str)]) -> Vec<DocComment> {
                     }
                 }
             } else {
-                tags_buffer.push(SingleDocComment {
+                single_tags.push(SingleDocComment {
                     offset: start_offset,
                     tag: String::from("notice"),
                     value: line.trim().to_owned(),
@@ -96,11 +97,11 @@ pub fn tags(lines: &[(usize, CommentType, &str)]) -> Vec<DocComment> {
         }
 
         match ty {
-            CommentType::Line if tags_buffer.len() > 0 => tags.push(DocComment::Line {
-                comment: tags_buffer[0].to_owned(),
+            CommentType::Line if single_tags.len() > 0 => tags.push(DocComment::Line {
+                comment: single_tags[0].to_owned(),
             }),
             CommentType::Block => tags.push(DocComment::Block {
-                comments: tags_buffer,
+                comments: single_tags,
             }),
             _ => {}
         }
