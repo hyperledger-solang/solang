e910fb07f1d3f4e3cb2606b3eeab638a1d5b871f
Lucas Steuernagel <lucas.tnagel@gmail.com>
2022-03-01 09:52:04 -0300

    Add tests for msg.sender system call
    Signed-off-by: Lucas Steuernagel <lucas.tnagel@gmail.com>

--- tests/substrate_tests/builtins.rs
+++ tests/substrate_tests/builtins.rs
@@ -677,3 +677,32 @@ fn mulmod() {
 
     runtime.function("test", Vec::new());
 }
+
+#[test]
+fn my_token() {
+    #[derive(Debug, PartialEq, Encode, Decode)]
+    struct TokenTest([u8; 32], bool);
+    let mut runtime = build_solidity(
+        "
+        contract mytoken {
+            function test(address account, bool sender) public view returns (address) {
+                if (sender) {
+                    return msg.sender;
+                }
+                return account;
+            }
+        }
+        ",
+    );
+
+    let addr: [u8; 32] = [
+        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
+        0x16, 0x17, 0x18, 0x19, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x30,
+        0x31, 0x32,
+    ];
+    runtime.function("test", TokenTest(addr, true).encode());
+    assert_eq!(&runtime.vm.caller[..], &runtime.vm.output[..]);
+
+    runtime.function("test", TokenTest(addr, false).encode());
+    assert_eq!(&runtime.vm.output[..], &addr[..]);
+}
