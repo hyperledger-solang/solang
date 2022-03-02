7b2010c3d0a366f4f45fab49f18aa5b28f97a7cb
Sean Young <sean@mess.org>
2022-03-02 09:48:25 +0000

    Update files names now we have a release with linux arm64
    Signed-off-by: Sean Young <sean@mess.org>

--- vscode/src/utils/getPlatform.ts
+++ vscode/src/utils/getPlatform.ts
@@ -4,8 +4,9 @@ export default function getPlatform(): string | undefined {
     case 'arm64 win32':
       return 'solang.exe';
     case 'x64 linux':
+      return 'solang-linux-x86-64';
     case 'arm64 linux':
-      return 'solang-linux';
+      return 'solang-linux-arm64';
     case 'x64 darwin':
       return 'solang-mac-intel';
     case 'arm64 darwin':
