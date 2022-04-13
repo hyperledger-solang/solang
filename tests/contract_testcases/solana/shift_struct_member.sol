d401d8c71fff5d1e38f6f550f43c513f5e02e9b7
Sean Young <sean@mess.org>
2022-04-13 10:14:56 +0100

    Shift on integer struct member causes panic
    The syntax `self._x >> 112` causes a panic because retrieving the bit
    width of a referene to an integer is broken.
    
    Fixes issue #732.
    
    Signed-off-by: Sean Young <sean@mess.org>

---
+++ tests/contract_testcases/solana/shift_struct_member.sol
@@ -0,0 +1,21 @@
+// Test case for https://github.com/hyperledger-labs/solang/issues/732
+library FixedPoint {
+   
+    struct uq144x112 {
+        uint _x;
+    }
+
+    struct uq112x112 {
+        uint224 _x;
+    }
+
+    // this errors with `type not allowed Ref(Uint(224))`
+    function decode(uq112x112 memory self) internal pure returns (uint112) {
+        return uint112(self._x >> 112);
+    }
+
+    // this error with `type not allowed Ref(Uint(256))`
+    function decode144(uq144x112 memory self) internal pure returns (uint144) {
+        return uint144(self._x >> 112);
+    }
+}
