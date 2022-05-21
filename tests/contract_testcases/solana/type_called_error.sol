b17ead4c42498a66c6d46e2f0cd16a5384afb385
Sean Young <sean@mess.org>
2022-05-21 08:42:07 +0100

    Allow types to be called error
    solc allows this too.
    
    Found in https://github.com/hyperledger-labs/solang/pull/787
    
    Signed-off-by: Sean Young <sean@mess.org>

---
+++ tests/contract_testcases/solana/type_called_error.sol
@@ -0,0 +1,16 @@
+struct error { int f1; }
+
+function member(error e) pure returns (int) {
+	return e.f1;
+}
+
+using {member} for error;
+
+contract c {
+	error public v1;
+
+	function test(error e) internal pure returns (error) {
+		e.member();
+		return e;
+	}
+}
