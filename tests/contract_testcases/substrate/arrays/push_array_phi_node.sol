5387b77d96fe5ea99f1fa358d41da6790759679f
Sean Young <sean@mess.org>
2022-06-24 16:03:50 +0100

    Ensure that codegen is correct for push/pop in if statements (#893)
    A push or pop may result in a realloc, so we need a phi node for the
    array variable.
    
    See https://discord.com/channels/905194001349627914/905834552965103638/989739679320535050
    
    Signed-off-by: Sean Young <sean@mess.org>

---
+++ tests/contract_testcases/substrate/arrays/push_array_phi_node.sol
@@ -0,0 +1,18 @@
+contract Array_bound_Test {
+    function array_bound() public pure returns (uint256) {
+        uint256[] a = new uint256[](10);
+        uint256 sum = 0;
+
+        if (2>1) {
+	    a.push(5);
+	} else {
+	    a.pop();
+	}
+
+        for (uint256 i = 0; i < a.length; i++) {
+	    sum = sum + a[10];
+	}
+
+        return sum;
+    }
+}
