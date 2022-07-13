1fe788d20159e693061823071b4aa48566f322e9
Lucas Steuernagel <38472950+LucasSte@users.noreply.github.com>
2022-07-13 11:34:11 -0300

    Fix load of vector of strings from storage on Solana (#918)
    Signed-off-by: Lucas Steuernagel <lucas.tnagel@gmail.com>

--- tests/solana_tests/strings.rs
+++ tests/solana_tests/strings.rs
@@ -30,3 +30,41 @@ fn storage_string_length() {
 
     assert_eq!(returns[0], Token::Uint(U256::from(18)));
 }
+
+#[test]
+fn load_string_vector() {
+    let mut vm = build_solidity(
+        r#"
+    contract Testing {
+        string[] string_vec;
+        function testLength() public returns (uint32, uint32, uint32) {
+            string_vec.push("tea");
+            string_vec.push("coffe");
+            string_vec.push("sixsix");
+            string[] memory rr = string_vec;
+            return (rr[0].length, rr[1].length, rr[2].length);
+        }
+
+        function getString(uint32 index) public view returns (string memory) {
+            string[] memory rr = string_vec;
+            return rr[index];
+        }
+    }
+      "#,
+    );
+
+    vm.constructor("Testing", &[]);
+    let returns = vm.function("testLength", &[], &[], None);
+    assert_eq!(returns[0], Token::Uint(U256::from(3)));
+    assert_eq!(returns[1], Token::Uint(U256::from(5)));
+    assert_eq!(returns[2], Token::Uint(U256::from(6)));
+
+    let returns = vm.function("getString", &[Token::Uint(U256::from(0))], &[], None);
+    assert_eq!(returns[0], Token::String("tea".to_string()));
+
+    let returns = vm.function("getString", &[Token::Uint(U256::from(1))], &[], None);
+    assert_eq!(returns[0], Token::String("coffe".to_string()));
+
+    let returns = vm.function("getString", &[Token::Uint(U256::from(2))], &[], None);
+    assert_eq!(returns[0], Token::String("sixsix".to_string()));
+}
