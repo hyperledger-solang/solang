21fa06d53e9ee7edbf4eb26b9ef0f1fb8ad8be08
Cyrill Leutwiler <bigcyrill@hotmail.com>
2022-09-28 20:45:03 +0200

    seal_call flags can be done later
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- src/emit/substrate/target.rs
+++ src/emit/substrate/target.rs
@@ -1192,7 +1192,7 @@ impl<'a> TargetRuntime<'a> for SubstrateTarget {
         let ret = call!(
             "seal_call",
             &[
-                i32_null!().into(), // TODO implement flags
+                i32_null!().into(), // TODO implement flags (mostyl used for proxy calls)
                 address.unwrap().into(),
                 gas.into(),
                 cast_byte_ptr!(value_ptr, "value_transfer").into(),
