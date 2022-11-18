7b800f2b516cffad57d61e2e3b95892d7761b7fe
xermicus <cyrill@parity.io>
2022-11-18 17:21:38 +0100

    set up test logic
    Signed-off-by: xermicus <cyrill@parity.io>

--- tests/doc_examples.rs
+++ tests/doc_examples.rs
@@ -1,13 +1,10 @@
-use solang::Target;
+use rayon::prelude::*;
+use solang::{file_resolver::FileResolver, sema::diagnostics::Diagnostics, Target};
+use std::{ffi::OsStr, fs::read_to_string};
 use walkdir::WalkDir;
 
-//const CODE_BLOCK: Lazy<Mutex<Regex>> = Lazy::new(|| {
-//    Regex::new(r"\.\. code-block:: solidity:[\s\S]+?(?=\n\n\S)")
-//        .unwrap()
-//        .into()
-//});
-
-fn doc_example_files(dir: &str) -> Vec<String> {
+// Returns a list of all `.sol` files in the given `dir` path.
+fn get_source_files(dir: &str) -> Vec<String> {
     WalkDir::new(dir)
         .into_iter()
         .filter_map(|entry| {
@@ -22,13 +19,37 @@ fn doc_example_files(dir: &str) -> Vec<String> {
         .collect()
 }
 
-fn assert_compile(path: &str, target: Target) {
-    todo!()
+// Attempts to compile the file at `path` for the `target`, returning the diagnostics on fail.
+fn try_compile(path: &str, target: Target) -> Result<(), Diagnostics> {
+    let mut cache = FileResolver::new();
+    cache.set_file_contents(path, read_to_string(path).unwrap());
+    let diagnostics = solang::compile(
+        OsStr::new(path),
+        &mut cache,
+        inkwell::OptimizationLevel::Default,
+        target,
+        true,
+        false,
+    )
+    .1
+    .diagnostics;
+    if diagnostics.any_errors() {
+        return Err(diagnostics);
+    }
+    Ok(())
+}
+
+// Assert no compilation errors for for all `.sol` files found in `dir`.
+fn assert_compile(dir: &str, target: Target) {
+    let errors = get_source_files(dir)
+        .par_iter()
+        .filter_map(|path| try_compile(path, target).err().map(|msg| (path, msg)))
+        .map(|(path, msg)| println!("{} failed: {:#?}", path, msg))
+        .count();
+    assert_eq!(0, errors);
 }
 
 #[test]
 fn they_compile() {
-    for p in doc_example_files("examples/") {
-        println!("{p}")
-    }
+    assert_compile("examples/", Target::default_substrate())
 }
