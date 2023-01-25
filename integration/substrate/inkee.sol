76f0ffb44795334401c334eba69e43671628a3e1
Cyrill Leutwiler <cyrill@parity.io>
2023-01-25 15:36:49 +0100

    Adapt ink cross call test to new interface (#1139)

--- integration/substrate/inkee.sol
+++ integration/substrate/inkee.sol
@@ -1,6 +1,6 @@
 contract Inkee {
     @selector([1, 2, 3, 4])
-    function echo(uint32 v) public pure returns (uint32) {
-        return v;
+    function echo(uint32 v) public pure returns (bool, uint32) {
+        return (false, v);
     }
 }
