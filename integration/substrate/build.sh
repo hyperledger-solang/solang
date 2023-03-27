9cf83f4be17a17964931962080231c1f3be36675
Cyrill Leutwiler <bigcyrill@hotmail.com>
2023-03-27 12:39:53 +0200

    fix merge fuckup
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- integration/substrate/build.sh
+++ integration/substrate/build.sh
@@ -6,6 +6,6 @@ if [[ $dup_contracts ]]; then
 	echo "Found contract with duplicate names: ${dup_contracts}"
 	/bin/false
 else
-	parallel solang compile -v --target substrate --log-runtime-errors --math-overflow ::: *.sol test/*.sol;  solang compile debug_buffer_format.sol --target substrate -v --log-runtime-errors --log-api-return-codes
+	parallel solang compile -v -g --target substrate --log-runtime-errors --math-overflow --log-api-return-codes ::: *.sol test/*.sol
 fi
 
