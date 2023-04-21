025d582f1b15dc00ee70d0003479e75e9c46eb95
Cyrill Leutwiler <bigcyrill@hotmail.com>
2023-04-21 16:47:34 +0200

    quote define function works
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- tests/wasm_host_derive/src/lib.rs
+++ tests/wasm_host_derive/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
-use syn::{punctuated::Punctuated, token::Comma, Ident, ImplItem, ItemImpl};
+use syn::{punctuated::Punctuated, token::Comma, Ident, ImplItem, ItemImpl, Type};
 
 #[derive(Debug)]
 struct HostFn {
@@ -25,7 +25,7 @@ impl HostFn {
             .find(|attr| attr.path().get_ident().unwrap() == "link")
             .map(|attr| attr.parse_args::<Ident>().unwrap().to_string())?;
 
-        // Collect the actual inputs of the host function
+        // The first 2 inputs of the impl function do not belong to the wasm host function
         let params = item
             .sig
             .inputs
@@ -41,24 +41,41 @@ impl HostFn {
             block: item.block.clone().into_token_stream(),
         })
     }
+
+    fn to_tokens(&self, host_ty: &Box<Type>) -> TokenStream2 {
+        let block = &self.block;
+        let params = &self.params;
+        let name = &self.name;
+        let module = &self.module;
+
+        quote!(
+            linker.define(#module, #name, ::wasmi::Func::wrap(
+                store, |mut __ctx__: ::wasmi::Caller<#host_ty>, #params| {
+                    let mem = __ctx__.data().call_stack.last().unwrap().memory;
+                    let (mem, vm) = mem.data_and_store_mut(&mut __ctx__);
+                    #block
+                }
+            ));
+        )
+    }
 }
 
 #[proc_macro_attribute]
 pub fn wasm_host(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let item = syn::parse_macro_input!(item as ItemImpl);
-    let ty = item.self_ty; // Ignoring generics, we don't need them
-    let host_functions = item
+    let host_ty = item.self_ty; // Ignoring generics, we don't need them
+    let impls = item
         .items
         .iter()
         .filter_map(HostFn::new)
-        .map(|f| dbg!(f))
-        .collect::<Vec<_>>();
+        .map(|f| f.to_tokens(&host_ty));
 
-    quote!(impl #ty {
+    quote!(impl #host_ty {
         fn define(
-            store: &mut ::wasmi::Store<#ty>,
-            linker: &mut ::wasmi::Linker<#ty>
+            store: &mut ::wasmi::Store<#host_ty>,
+            linker: &mut ::wasmi::Linker<#host_ty>
         ) -> Result<(),::wasmi::errors::LinkerError> {
+            #( #impls )*
             Ok(())
         }
     })
