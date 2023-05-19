1a47714956873b610450741ff97de4538be1e7c8
Sean Young <sean@mess.org>
2023-05-19 19:30:13 +0100

    `this` is not a keyword (#1317)
    this should not be a keyword, and variables can be named this although this will hide the usual this which gives the current contract.
    
    Signed-off-by: Sean Young <sean@mess.org>

--- tests/contract_testcases/substrate/inheritance/test_super.sol
+++ tests/contract_testcases/substrate/inheritance/test_super.sol
@@ -1,3 +1,4 @@
 contract super {}
 // ---- Expect: diagnostics ----
-// error: 1:10-15: 'super' shadows name of a builtin
+// error: 1:1-18: contracts without public storage or functions are not allowed on Substrate. Consider declaring this contract abstract: 'abstract contract super'
+// warning: 1:10-15: 'super' shadows name of a builtin
