102728a29064e99e899455df29457ea16a4f6362
Sean Young <sean@mess.org>
2023-06-16 19:20:16 +0100

    Allow returns values to be ignored in try catch
    A try catch statement does not need to decode the return values if it is not interested.
    
    Signed-off-by: Sean Young <sean@mess.org>

---
+++ tests/codegen_testcases/solidity/try_catch.sol
@@ -0,0 +1,17 @@
+// RUN: --target substrate --emit cfg
+interface I {
+	function bar() external returns (int32, bool);
+}
+
+contract C {
+// BEGIN-CHECK: C::C::function::foo
+	function foo(I i) public returns (int32 x) {
+        try i.bar() {
+            // ensure no abi decoding is done because the return values of bar() are not used
+            x = 1;
+        } catch (bytes) {
+            x = 2;
+        }
+// NOT-CHECK: builtin ReadFromBuffer ((external call return data)
+	}
+}
