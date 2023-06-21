ce8421d455424a6b5473003c50a96c5878974908
xermicus <cyrill@parity.io>
2023-06-21 21:13:50 +0200

    simplify the docs example
    Signed-off-by: xermicus <cyrill@parity.io>

--- docs/examples/substrate/call_flags.sol
+++ docs/examples/substrate/call_flags.sol
@@ -1,23 +1,16 @@
-contract CallFlags {
+library CallFlags {
+    uint32 constant FORWARD_INPUT = 1;
+    uint32 constant CLONE_INPUT = 2;
+    uint32 constant TAIL_CALL = 4;
+    uint32 constant ALLOW_REENTRY = 8;
+}
 
-    enum CallFlag { FORWARD_INPUT, CLONE_INPUT, TAIL_CALL, ALLOW_REENTRY }
-
-    // Set all flags found in _flags to true.
-    function bitflags(CallFlag[] _flags) internal pure returns (uint32 flags) {
-        for (uint n = 0; n < _flags.length; n++) {
-            flags |= (2 ** uint32(_flags[n]));
-        }
-    }
-
-    // Call the contract at _address with the given _selector.
-    // Specify any flag used for the contract call in _flags.
-    function call_with_flags(
+contract Reentrant {
+    function reentrant_tail_call(
         address _address,
-        bytes4 _selector,
-        CallFlag[] _flags
+        bytes4 selector
     ) public returns (bytes ret) {
-        uint32 call_flags = bitflags(_flags);
-        (bool ok, ret) = _address.call{flags: call_flags}(_selector);
+        (bool ok, ret) = _address.call{flags: CallFlags.ALLOW_REENTRY}(selector);
         require(ok);
     }
 }
