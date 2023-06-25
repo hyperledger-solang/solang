d50313144f87bc90f3b9b1f163f37f5dfcf92356
xermicus <cyrill@parity.io>
2023-06-25 13:46:27 +0200

    improve codegen test
    Signed-off-by: xermicus <cyrill@parity.io>

--- tests/codegen_testcases/solidity/modifier_opt.sol
+++ tests/codegen_testcases/solidity/modifier_opt.sol
@@ -1,13 +1,22 @@
 // RUN: --target substrate --release --emit cfg
+
 contract Test {
-    modifier modified() {
+    function testMethod() public payable m1 m2 returns (uint256) {
+        return 2 ** 256 - 1;
+        // CHECK: block0: # entry
+        // CHECK: return uint256 -1
+        // NOT-CHECK: return ((uint256 2 ** uint256 256) - uint256 1)
+    }
+
+    modifier m1() {
         _;
     }
 
-    function testMethod() public pure modified returns (uint256) {
-        return 2 ** 256 - 1;
+    modifier m2() {
+        _;
         // CHECK: block0: # entry
-        // NOT-CHECK: return ((uint256 2 ** uint256 256) - uint256 1)
-        // CHECK: return uint256 -1
+        // CHECK: branchcond (uint128((builtin Value ())) != uint128 -1), block1, block2
+        // NOT-CHECK: branchcond (uint128((builtin Value ())) != (uint128 2 ** uint128 127)), block1, block2
+        require(msg.value != 2 ** 128 - 1);
     }
 }
