055a5c94bca3f23b851673b8adb51500297bdef9
Sean Young <sean@mess.org>
2023-07-10 10:02:05 +0100

    Update solana-library/minimum_balance.sol
    Co-authored-by: Lucas Steuernagel <38472950+LucasSte@users.noreply.github.com>
    Signed-off-by: Sean Young <sean@mess.org>

--- solana-library/minimum_balance.sol
+++ solana-library/minimum_balance.sol
@@ -23,7 +23,7 @@ uint64 constant DEFAULT_EXEMPTION_THRESHOLD = 2;
 /// Account storage overhead for calculation of base rent.
 ///
 /// This is the number of bytes required to store an account with no data. It is
-/// added to an accounts data length when calculating [`Rent::minimum_balance`].
+/// added to an account's data length when calculating [`Rent::minimum_balance`].
 uint64 constant ACCOUNT_STORAGE_OVERHEAD = 128;
 
 /// Minimum balance due for rent-exemption of a given account data size.
