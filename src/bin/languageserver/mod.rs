760d70d504601dcba85a7b95eb74af0b8d8a732f
Govardhan G D <chioni1620@gmail.com>
2023-09-17 10:12:47 +0530

    error handling + comments
    Signed-off-by: Govardhan G D <chioni1620@gmail.com>

--- src/bin/languageserver/mod.rs
+++ src/bin/languageserver/mod.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
+use forge_fmt::{format, parse, FormatterConfig};
 use itertools::Itertools;
 use num_traits::ToPrimitive;
 use rust_lapper::{Interval, Lapper};
@@ -38,8 +39,6 @@ use tower_lsp::{
     },
     Client, LanguageServer, LspService, Server,
 };
-
-use forge_fmt::{format, parse, FormatterConfig};
 
 use crate::cli::{target_arg, LanguageServerCommand};
 
@@ -2138,19 +2137,60 @@ impl LanguageServer for SolangServer {
         Ok(locations)
     }
 
+    /// Called when "Format Document" is called by the user on the client side.
+    ///
+    /// Expected to return the formatted version of source code present in the file on which this method was triggered.
+    ///
+    /// ### Arguments
+    /// * `DocumentFormattingParams`
+    ///     * provides the name of the file whose code is to be formatted.
+    ///     * provides options that help configure how the file is formatted.
+    ///
+    /// ### Edge cases
+    /// * Returns `Err` when
+    ///     * an invalid file path is received.
+    ///     * reading the file fails.
+    ///     * parsing the file fails.
+    ///     * formatting the file fails.
     async fn formatting(&self, params: DocumentFormattingParams) -> Result<Option<Vec<TextEdit>>> {
-        let source_path = params.text_document.uri.to_file_path().unwrap();
-        let source = std::fs::read_to_string(source_path).unwrap();
-        let source_parsed = parse(&source).unwrap();
+        // get parse tree for the input file
+        let uri = params.text_document.uri;
+        let source_path = uri.to_file_path().map_err(|_| Error {
+            code: ErrorCode::InvalidRequest,
+            message: format!("Received invalid URI: {uri}").into(),
+            data: None,
+        })?;
+        let source = std::fs::read_to_string(source_path).map_err(|err| Error {
+            code: ErrorCode::InternalError,
+            message: format!("Failed to read file: {uri}").into(),
+            data: Some(Value::String(format!("{:?}", err))),
+        })?;
+        let source_parsed = parse(&source).map_err(|err| {
+            let err = err
+                .into_iter()
+                .map(|e| Value::String(e.message))
+                .collect::<Vec<_>>();
+            Error {
+                code: ErrorCode::InternalError,
+                message: format!("Failed to parse file: {uri}").into(),
+                data: Some(Value::Array(err)),
+            }
+        })?;
 
+        // get the formatted text
         let config = FormatterConfig {
             line_length: 80,
+            tab_width: params.options.tab_size as _,
             ..Default::default()
         };
-
         let mut source_formatted = String::new();
-        format(&mut source_formatted, source_parsed, config).unwrap();
+        format(&mut source_formatted, source_parsed, config).map_err(|err| Error {
+            code: ErrorCode::InternalError,
+            message: format!("Failed to format file: {uri}").into(),
+            data: Some(Value::String(format!("{:?}", err))),
+        })?;
 
+        // create a `TextEdit` instance that replaces the contents of the file with the formatted text
         let text_edit = TextEdit {
             range: Range {
                 start: Position {
