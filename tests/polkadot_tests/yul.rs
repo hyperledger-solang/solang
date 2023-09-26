e3df35f4ce7ad5e019dbaa094b4b10f87a27f75e
Cyrill Leutwiler <bigcyrill@hotmail.com>
2023-09-26 18:56:44 +0200

    Bugfix: Return parameters can have storage a location
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- tests/polkadot_tests/yul.rs
+++ tests/polkadot_tests/yul.rs
@@ -257,3 +257,56 @@ contract Testing {
     runtime.function("switch_no_case", Val256(U256::from(3)).encode());
     assert_eq!(runtime.output(), Val256(U256::from(4)).encode());
 }
+
+#[test]
+fn storage_slot() {
+    let mut runtime = build_solidity(
+        r#"
+    library StorageSlot {
+        struct AddressSlot {
+            address value;
+        }
+    
+        function getAddressSlot(
+            bytes32 slot
+        ) internal pure returns (AddressSlot storage r) {
+            assembly {
+                r.slot := slot
+            }
+        }
+    }
+    
+    contract StorageKeys {
+        function set(bytes32 key) public {
+            StorageSlot.AddressSlot storage slot = StorageSlot.getAddressSlot(key);
+            slot.value = msg.sender;
+        }
+    
+        function get(bytes32 key) public returns (address a) {
+            a = StorageSlot.getAddressSlot(key).value;
+        }
+    }"#,
+    );
+
+    let mut key = [0u8; 32];
+
+    runtime.function("get", key.to_vec());
+    let zero = runtime.output();
+
+    key[0] = 1;
+    runtime.function("get", key.to_vec());
+    let one = runtime.output();
+
+    key[0] = 2;
+    runtime.function("get", key.to_vec());
+    let two = runtime.output();
+
+    // Without setting any key, they all shuld be default
+    assert_eq!(zero, one);
+    assert_eq!(zero, two);
+
+    // Setting the key should write to the correct storage slot
+    runtime.function("set", key.to_vec());
+    runtime.function("get", key.to_vec());
+    assert_eq!(runtime.output(), runtime.caller())
+}
