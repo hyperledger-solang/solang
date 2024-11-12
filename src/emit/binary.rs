73928beb2561299474120d10d40482441c83f6d7
salaheldinsoliman <salaheldin_sameh@aucegypt.edu>
2024-11-12 07:51:43 +0100

    address soroban specifc void type
    Signed-off-by: salaheldinsoliman <salaheldin_sameh@aucegypt.edu>

--- src/emit/binary.rs
+++ src/emit/binary.rs
@@ -1016,7 +1016,13 @@ impl<'a> Binary<'a> {
                     self.llvm_type(&Type::Bytes(ns.target.selector_length()), ns)
                 }
                 // Soroban functions always return a 64 bit value.
-                Type::Void => BasicTypeEnum::IntType(self.context.i64_type()),
+                Type::Void => {
+                    if ns.target == Target::Soroban {
+                        BasicTypeEnum::IntType(self.context.i64_type())
+                    } else {
+                        unreachable!()
+                    }
+                }
                 _ => unreachable!(),
             }
         }
