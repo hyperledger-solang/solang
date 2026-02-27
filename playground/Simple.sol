795921eb405fdf0c41e7885967db4a10e7d26bd4
0takuc0mrade <junep059@gmail.com>
2026-02-27 22:18:15 +0100

    chore: drop playground/generated artifacts from branch
    Signed-off-by: 0takuc0mrade <junep059@gmail.com>

--- playground/Simple.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Simple {
-    function add(uint64 a, uint64 b) public pure returns (uint64) {
-        return a + b;
-    }
-}
--- playground/test.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-import "polkadot";
-
-contract Foo {
-    function chain_extension() public pure {}
-}
